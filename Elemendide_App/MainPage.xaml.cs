750812fb74eb8c9823d575f40b0a511c65dfb848
Opilane TTHK <opilane@tthk.ee>
2022-02-10 15:32:31 +0200

    Add project files.

---
+++ Elemendide_App/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Elemendide_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            //InitializeComponent();
+            StackLayout st = new StackLayout();
+            Button Ent_btn = new Button()
+            {
+                Text = "Entry",
+                BackgroundColor = Color.LightGreen,
+            };
+
+            Button Timer_btn = new Button()
+            {
+                Text = "Timer",
+                BackgroundColor = Color.LightGreen,
+            };
+            Button cliker = new Button()
+            {
+                Text = "Timer",
+                BackgroundColor = Color.LightGreen,
+            };
+            Button Date_btn = new Button
+            {
+                Text = "Date/Time",
+                BackgroundColor = Color.Fuchsia
+            };
+            Button SS_btn = new Button
+            {
+                Text = "Stepper/Slider",
+                BackgroundColor = Color.IndianRed
+            };
+
+
+            st.Children.Add(Ent_btn);
+            st.Children.Add(Timer_btn);
+            st.Children.Add(cliker);
+            st.BackgroundColor = Color.AntiqueWhite;
+            Content = st;
+            Ent_btn.Clicked += Ent_btn_Clicked;
+            Timer_btn.Clicked += Timer_btn_Clicked;
+            cliker.Clicked += Cliker_Clicked;
+        }
+
+        private async void Cliker_Clicked(object sender, EventArgs e)
+        {
+             await Navigation.PushAsync(new cliker());
+        }
+
+        private async void Timer_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TimerPage());
+        }
+
+        private async void Ent_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Ent_page());
+        }
+        private async void Date_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Ent_page());
+        }
+        private async void SS_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Ent_page());
+        }
+    }
+    
+}
