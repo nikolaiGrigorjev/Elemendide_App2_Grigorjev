50eb6a08ded222635e70dc333ee0172d0385b9d9
opilane <opilane@HP-CZC6297B98>
2022-02-11 14:08:38 +0200

    fdgfdf

--- Elemendide_App/MainPage.xaml.cs
+++ Elemendide_App/MainPage.xaml.cs
@@ -35,11 +35,23 @@ namespace Elemendide_App
                 Text = "Date/Time",
                 BackgroundColor = Color.Fuchsia
             };
-            Button SS_btn = new Button
+
+            Button Frame_btn = new Button
             {
-                Text = "Stepper/Slider",
+                Text = "Frame",
                 BackgroundColor = Color.IndianRed
             };
+            Button Image_btn = new Button
+            {
+                Text = "Image",
+                BackgroundColor = Color.IndianRed
+            };
+            Button valgusfoor_btn = new Button
+            {
+                Text = "Valgusfoor",
+                BackgroundColor = Color.IndianRed
+            };
+
 
 
             st.Children.Add(Ent_btn);
@@ -50,6 +62,24 @@ namespace Elemendide_App
             Ent_btn.Clicked += Ent_btn_Clicked;
             Timer_btn.Clicked += Timer_btn_Clicked;
             cliker.Clicked += Cliker_Clicked;
+            Frame_btn.Clicked += Frame_btn_Clicked;
+            Image_btn.Clicked += Image_btn_Clicked;
+            valgusfoor_btn.Clicked += Valgusfoor_btn_Clicked;
+        }
+
+        private async void Valgusfoor_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new valgusfoor());
+        }
+
+        private async void Image_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Image_page());
+        }
+
+        private async void Frame_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Frame_Page());
         }
 
         private async void Cliker_Clicked(object sender, EventArgs e)
